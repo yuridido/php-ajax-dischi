c695f971579671f9cb83dfc488a13703a6749f49
yuridido <yuridido@yahoo.it>
2020-09-18 16:43:06 +0200

    stampa dischi

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+include __DIR__ . '/src/db.php';
+
+
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="dist/app.css">
+        <title></title>
+    </head>
+    <body>
+        <header>
+
+        </header>
+        <main>
+            <div class="container">
+                <?php foreach ($database as $disco) {
+                    // code...
+                ?>
+                <div class="disco">
+                    <img src="<?php echo $disco['poster'] ?>" alt="foto disco">
+                    <h2><?php echo $disco['author'] ?></h2>
+                    <p><?php echo $disco['year'] ?></p>
+                    <p><?php echo $disco['title'] ?></p>
+                </div>
+                <?php } ?>
+            </div>
+        </main>
+    </body>
+</html>
